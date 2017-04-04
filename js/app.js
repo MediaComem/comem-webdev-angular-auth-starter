a9c2015e3e96ffa3896696caff620173ea0d6396
Mathias Oberson <mathias.oberson@heig-vd.ch>
2017-04-04 14:52:27 +0200

    Added the interceptor.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,7 @@ angular.module('app', [
     'angular-storage'
 ]);
 
-angular
-    .module('app')
-    .config(AppConfig)
-    .run(AppRun);
-
-function AppConfig($stateProvider, $urlRouterProvider) {
+angular.module('app').config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $stateProvider.state('login', {
         url: '/login',
         templateUrl: './templates/login.html',
@@ -25,16 +20,18 @@ function AppConfig($stateProvider, $urlRouterProvider) {
         templateUrl: './templates/second.html'
     });
 
-    $urlRouterProvider.otherwise(function($injector) {
+    $urlRouterProvider.otherwise(function ($injector) {
         $injector.get('$state').go('home');
     });
-}
 
-function AppRun(AuthService, $rootScope, $state) {
-    $rootScope.$on('$stateChangeStart', function(event, toState) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+})
+
+angular.module('app').run(function (AuthService, $rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function (event, toState) {
         if (!AuthService.token && toState.name !== 'login') {
             event.preventDefault();
             $state.go('login');
         }
     });
-}
+});
